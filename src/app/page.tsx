11bfdc0ddee075a56da8d62c5800900222ce84eb
tanto <tantows001@gmail.com>
2024-05-25 02:16:25 +0700

    feat: animate background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
+import { PartcilesComponent } from "@/components/particles";
 
 export default function Home() {
   useEffect(() => {
@@ -15,68 +16,71 @@ export default function Home() {
     });
   }, [])
   return (
-    <main className="text-white">
-      <section>
-        <div className="flex flex-wrap items-center max-w-7xl mx-auto px-8 lg:px-24 md:px-16 pt-16 pb-8 lg:pt-36">
-          <div data-aos="zoom-in" data-aos-duration='800' className="flex flex-wrap justify-center py-8 w-full md:w-1/3 md:pr-16 relative">
-            <div className="relative h-96 w-72">
-              <div className="bg-[#054A45] p-4 h-full rounded-xl relative"></div> 
-              <div className="border border-white h-full rounded-xl absolute inset-0 rotate-12"></div>
-              <Image
-                src={'/img/foto_diri.png'}
-                width={300}
-                height={300}
-                className="absolute top-0 left-0 w-full h-96" alt={"foto diri"}/> 
+    <main>
+      <PartcilesComponent />
+      <div className="text-white">
+        <section>
+          <div className="flex flex-wrap items-center max-w-7xl mx-auto px-8 lg:px-24 md:px-16 pt-16 pb-8 lg:pt-36">
+            <div className="flex flex-wrap justify-center py-8 w-full md:w-1/3 md:pr-16 relative">
+              <div className="relative h-96 w-72">
+                <div className="bg-[#054A45] p-4 h-full rounded-xl relative"></div> 
+                <div className="border border-white h-full rounded-xl absolute inset-0 rotate-12"></div>
+                <Image
+                  src={'/img/foto_diri.png'}
+                  width={300}
+                  height={300}
+                  className="absolute top-0 left-0 w-full h-96" alt={"foto diri"}/> 
+              </div>
+            </div>
+            <div className="w-full md:w-2/3">
+              <div className="flex flex-wrap flex-col gap-6 mb-8">
+                <h4 className="text-[#054A45] font-bold text-2xl">Hi, I&apos;m Shyra Athaya</h4>
+                <h1 className="font-bold text-3xl md:text-4xl">UI/UX Designer</h1>
+                <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have experience in UI/UX designer and project management. Additionally, I possess strong communication, negotiation, teamwork skills, with a flexible mindset to adapt to any working environment, and also has experience in organizations.</p>
+              </div>
+              <div className="flex flex-wrap gap-4">
+                <a href="https://www.linkedin.com/in/shyrath3104/" target="_blank">
+                  <FaLinkedin className="text-4xl"/>
+                </a>
+                <a href="https://github.com/J0key" target="_blank">
+                  <FaGithub className="text-4xl"/>
+                </a>
+              </div>
             </div>
           </div>
-          <div data-aos="fade-up-left" data-aos-duration='800' data-aos-delay='500' className="w-full md:w-2/3">
-            <div className="flex flex-wrap flex-col gap-6 mb-8">
-              <h4 className="text-[#054A45] font-bold text-2xl">Hi, I&apos;m Shyra Athaya</h4>
-              <h1 className="font-bold text-3xl md:text-4xl">UI/UX Designer</h1>
-              <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have experience in UI/UX designer and project management. Additionally, I possess strong communication, negotiation, teamwork skills, with a flexible mindset to adapt to any working environment, and also has experience in organizations.</p>
-            </div>
-            <div className="flex flex-wrap gap-4">
-              <a href="https://www.linkedin.com/in/shyrath3104/" target="_blank">
-                <FaLinkedin className="text-4xl"/>
-              </a>
-              <a href="https://github.com/J0key" target="_blank">
-                <FaGithub className="text-4xl"/>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <div className="max-w-7xl mx-auto px-8 lg:px-24 md:px-16 py-16 lg:py-24">
-          <div className="flex flex-wrap justify-between w-full mb-2 md:mb-6">
-            <h1 className="font-bold text-3xl md:text-4xl">Experiences</h1>
-            <Link href={'/experience'} className="bg-[#054A45] rounded-full p-2 hover:bg-[#04423D]"><IoIosArrowForward className="text-xl md:text-2xl font-bold" /></Link>
-          </div>
-          <p className="mb-12">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, doloremque.</p>
-          <div data-aos="fade-up-right" data-aos-duration='800'  className="flex flex-wrap flex-col gap-2 md:gap-4 mb-8 md:mb-6">
-            <h4 className="font-bold text-2xl md:text-3xl bg-gradient-to-r from-[#054A45] to-white w-fit bg-clip-text text-transparent">UI/UX Designer</h4>            
-            <h2 className="font-bold text-md md:text-xl">December 2024 - December 2026</h2>
-            <div className="p-8 border border-[#054A45] max-w-lg rounded-lg hover:-translate-y-1 transition hover:shadow-xs hover:shadow-white">
-              <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have</p>
+        <section>
+          <div className="max-w-7xl mx-auto px-8 lg:px-24 md:px-16 py-16 lg:py-24">
+            <div className="flex flex-wrap justify-between w-full mb-2 md:mb-6">
+              <h1 className="font-bold text-3xl md:text-4xl">Experiences</h1>
+              <Link href={'/experience'} className="bg-[#054A45] rounded-full p-2 hover:bg-[#04423D]"><IoIosArrowForward className="text-xl md:text-2xl font-bold" /></Link>
+            </div>
+            <p className="mb-12">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, doloremque.</p>
+            <div data-aos="fade-up-right" data-aos-duration='800' className="flex flex-wrap flex-col gap-2 md:gap-4 mb-8 md:mb-6">
+              <h4 className="font-bold text-2xl md:text-3xl bg-gradient-to-r from-[#054A45] to-white w-fit bg-clip-text text-transparent">UI/UX Designer</h4>            
+              <h2 className="font-bold text-md md:text-xl">December 2024 - December 2026</h2>
+              <div className="p-8 border border-[#054A45] bg-[#1e1e1e] max-w-lg rounded-lg hover:-translate-y-1 transition shadow-md shadow-slate-800 hover:shadow-xl hover:shadow-slate-800">
+                <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have</p>
+              </div>
+            </div>
+            <div data-aos="fade-up-left" data-aos-duration='800' className="flex flex-wrap flex-col gap-2 md:gap-4 items-end mb-8 md:mb-6">
+              <h4 className="font-bold text-2xl md:text-3xl bg-gradient-to-r from-[#054A45] to-white w-fit bg-clip-text text-transparent">UI/UX Designer</h4>            
+              <h2 className="font-bold text-md md:text-xl">December 2024 - December 2026</h2>
+              <div className="p-8 border border-[#054A45] bg-[#1e1e1e] max-w-lg rounded-lg hover:-translate-y-1 transition shadow-md shadow-slate-800 hover:shadow-xl hover:shadow-slate-800">
+                <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have</p>
+              </div>
+            </div>
+            <div data-aos="fade-up-right" data-aos-duration='800' className="flex flex-wrap flex-col gap-2 md:gap-4">
+              <h4 className="font-bold text-2xl md:text-3xl bg-gradient-to-r from-[#054A45] to-white w-fit bg-clip-text text-transparent">UI/UX Designer</h4>            
+              <h2 className="font-bold text-md md:text-xl">December 2024 - December 2026</h2>
+              <div className="p-8 border border-[#054A45] bg-[#1e1e1e] max-w-lg rounded-lg hover:-translate-y-1 transition shadow-md shadow-slate-800 hover:shadow-xl hover:shadow-slate-800">
+                <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have</p>
+              </div>
             </div>
           </div>
-          <div data-aos="fade-up-left" data-aos-duration='800' className="flex flex-wrap flex-col gap-2 md:gap-4 items-end mb-8 md:mb-6">
-            <h4 className="font-bold text-2xl md:text-3xl bg-gradient-to-r from-[#054A45] to-white w-fit bg-clip-text text-transparent">UI/UX Designer</h4>            
-            <h2 className="font-bold text-md md:text-xl">December 2024 - December 2026</h2>
-            <div className="p-8 border border-[#054A45] max-w-lg rounded-lg hover:-translate-y-1 transition hover:shadow-xs hover:shadow-white">
-              <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have</p>
-            </div>
-          </div>
-          <div data-aos="fade-up-right" data-aos-duration='800' className="flex flex-wrap flex-col gap-2 md:gap-4">
-            <h4 className="font-bold text-2xl md:text-3xl bg-gradient-to-r from-[#054A45] to-white w-fit bg-clip-text text-transparent">UI/UX Designer</h4>            
-            <h2 className="font-bold text-md md:text-xl">December 2024 - December 2026</h2>
-            <div className="p-8 border border-[#054A45] max-w-lg rounded-lg hover:-translate-y-1 transition hover:shadow-xs hover:shadow-white">
-              <p className="text-justify">Highly motivated Software Engineering student with a strong foundation in user research and analyzing systems. I have</p>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
---
+++ src/components/particles.tsx
@@ -0,0 +1,96 @@
+import { useCallback, useMemo } from "react";
+import Particles from "react-tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
+export const PartcilesComponent = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+
+    const options = useMemo(
+        () => ({
+          background: {
+            color: {
+              value: "#1e1e1e",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "repulse",
+              },
+              onHover: {
+                enable: true,
+                mode: 'grab',
+              },
+            },
+            modes: {
+              push: {
+                distance: 200,
+                duration: 15,
+              },
+              grab: {
+                distance: 150,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#FFFFFF",
+            },
+            links: {
+              color: "#FFFFFF",
+              distance: 150,
+              enable: true,
+              opacity: 0.3,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: true,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 150,
+            },
+            opacity: {
+              value: 1.0,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 3 },
+            },
+          },
+          detectRetina: true,
+        }),
+        [],
+      );    
+
+    return (
+        <Particles className="-z-50 h-full w-full absolute" id="tsparticles" init={particlesInit} loaded={particlesLoaded} options={options} />
+    ); 
+};
